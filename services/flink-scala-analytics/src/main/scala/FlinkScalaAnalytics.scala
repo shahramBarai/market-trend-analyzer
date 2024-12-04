fbe8bf2bcb3bfd383019323188c8d174fb717163
Shahram Barai <shahram.barai@gmail.com>
2024-12-04 16:01:53 +0200

    Update docker-compose configuration and enhance Flink job setup
    - Added volume for images in docker-compose.dev.yml
    - Switched Flink job and task managers to use the latest image instead pyFlink image
    - Updated Kafka consumer properties for better integration
    - Introduced a custom timestamp extractor for FinancialTick

--- services/flink-scala-analytics/src/main/scala/FlinkScalaAnalytics.scala
+++ services/flink-scala-analytics/src/main/scala/FlinkScalaAnalytics.scala
@@ -1,4 +1,3 @@
-import org.apache.flink.api.common.serialization.{DeserializationSchema, SerializationSchema}
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
@@ -8,9 +7,20 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.util.Collector
+import org.apache.flink.api.common.serialization.{
+  DeserializationSchema,
+  SerializationSchema
+}
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import mypackage.message.{FinancialTick, AnalyticsResult}
 import org.apache.flink.api.scala.createTypeInformation
+import mypackage.message.{FinancialTick, AnalyticsResult}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+
+import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder}
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.temporal.ChronoField
 
 object RegionalMarketAnalytics {
   // Custom Protobuf deserializer for Kafka
@@ -21,7 +31,7 @@ object RegionalMarketAnalytics {
 
     override def isEndOfStream(nextElement: FinancialTick): Boolean = false
 
-    override def getProducedType: TypeInformation[FinancialTick] = 
+    override def getProducedType: TypeInformation[FinancialTick] =
       TypeInformation.of(classOf[FinancialTick])
   }
 
@@ -32,16 +42,59 @@ object RegionalMarketAnalytics {
     }
   }
 
-  def createJob(region: String, inputTopic: String, outputTopic: String, parallelism: Int): Unit = {
-    // Setup Flink execution environment
+  // Define the named timestamp extractor class
+  class FinancialTickTimestampExtractor
+      extends BoundedOutOfOrdernessTimestampExtractor[FinancialTick](
+        Time.seconds(10)
+      ) {
+
+    @transient private lazy val formatter = new DateTimeFormatterBuilder()
+      .appendPattern("yyyy-MM-dd HH:mm:ss")
+      .optionalStart()
+      .appendLiteral('.')
+      .appendFraction(ChronoField.NANO_OF_SECOND, 1, 9, true)
+      .optionalEnd()
+      .toFormatter()
+
+    override def extractTimestamp(element: FinancialTick): Long = {
+      try {
+        val localDateTime =
+          LocalDateTime.parse(element.tradingDateTime, formatter)
+        val timestamp = localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli
+        timestamp
+      } catch {
+        case e: Exception =>
+          // Log the error and handle it appropriately
+          println(
+            s"Error parsing timestamp '${element.tradingDateTime}': ${e.getMessage}"
+          )
+          0L // Return a default timestamp or handle as needed
+      }
+    }
+
+  }
+
+  def createJob(
+      region: String,
+      inputTopic: String,
+      outputTopic: String,
+      parallelism: Int
+  ): Unit = {
+    // Setup Flink execution environment, event time, and parallelism
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.setParallelism(parallelism)
 
     // Kafka consumer properties
     val kafkaConsumerProps = new java.util.Properties()
-    kafkaConsumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-    kafkaConsumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, s"flink-market-analytics-$region")
-    kafkaConsumerProps.put("compression.type", "snappy") // Enable Snappy compression
+    kafkaConsumerProps.put(
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+      "kafka:9092"
+    )
+    kafkaConsumerProps.put(
+      ConsumerConfig.GROUP_ID_CONFIG,
+      s"flink-market-analytics-$region"
+    )
 
     // Create Kafka consumer with Protobuf deserializer
     val kafkaConsumer = new FlinkKafkaConsumer[FinancialTick](
@@ -53,22 +106,36 @@ object RegionalMarketAnalytics {
     // Parse Market Tick Data
     val FinancialTickStream = env
       .addSource(kafkaConsumer)
-      .keyBy(_.id)
+      .assignTimestampsAndWatermarks(new FinancialTickTimestampExtractor())
 
     // Perform analytics in 1-minute tumbling windows
     val analyticsStream = FinancialTickStream
+      .keyBy(_.id)
       .timeWindow(Time.minutes(1))
       .apply { (key, window, input, out: Collector[AnalyticsResult]) =>
         // convert .last to a double and sum all prices
         val totalVolume = input.map(_.last).map(_.toDouble).sum
         val avgPrice = totalVolume / input.size
-        out.collect(AnalyticsResult(key, avgPrice.toString, totalVolume.toString, window.getEnd.toString))
+        out.collect(
+          AnalyticsResult(
+            key,
+            avgPrice.toString,
+            totalVolume.toString,
+            window.getEnd.toString
+          )
+        )
       }
 
     // Kafka producer properties
     val kafkaProducerProps = new java.util.Properties()
-    kafkaProducerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-    kafkaProducerProps.put("compression.type", "snappy") // Enable Snappy compression
+    kafkaProducerProps.put(
+      ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+      "kafka:9092"
+    )
+    kafkaProducerProps.put(
+      "compression.type",
+      "snappy"
+    )
 
     // Create Kafka producer with Protobuf serializer
     val kafkaProducer = new FlinkKafkaProducer[AnalyticsResult](
@@ -87,11 +154,11 @@ object RegionalMarketAnalytics {
   def main(args: Array[String]): Unit = {
     // Define regional jobs
     val regions = Seq(
-      ("Region1", "FR", "FR-analytics"),
+      ("Region1", "FR", "FR-analytics")
     )
 
     regions.foreach { case (region, inputTopic, outputTopic) =>
-      createJob(region, inputTopic, outputTopic, parallelism = 4)
+      createJob(region, inputTopic, outputTopic, parallelism = 1)
     }
   }
 }
